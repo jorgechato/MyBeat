dcbcb2231bd50bd2fdabdabb8746307fd19a5c4c
orggue <jorgechato1@gmail.com>
2015-06-01 14:02:24 +0200

    cleaning some pewp

--- app/src/main/java/org/jorgechato/mybeat/AddControl.java
+++ app/src/main/java/org/jorgechato/mybeat/AddControl.java
@@ -12,6 +12,7 @@ import android.media.MediaPlayer;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -24,6 +25,7 @@ import android.widget.TimePicker;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicResponseHandler;
@@ -50,8 +52,6 @@ public class AddControl extends Activity {
     private static TextView date,time;
     private static long calendarDate,calendarTime;
     private MediaPlayer mp;
-    private HttpClient client;
-    private HttpPost post;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,11 +68,6 @@ public class AddControl extends Activity {
         init();
         initSpinner();
         loadunit();
-    }
-
-    private void connection() {
-        client = new DefaultHttpClient();
-        post = new HttpPost("http://192.168.1.11:5000/api/controls/");
     }
 
     private void loadunit() {
@@ -168,8 +163,8 @@ public class AddControl extends Activity {
             Control control = new Control(note.getText().toString(), spinner.getSelectedItem().toString(),
                     spinnertype.getSelectedItem().toString(),new Date(calendarDate), new Time(calendarTime),
                     Integer.parseInt(String.valueOf(glucose.getText())), Integer.parseInt(String.valueOf(insulin.getText())));
-            database.newControl(postToAPI(control));
-
+            database.newControl(control);
+            postToAPI(control);
             mp.setLooping(false);
             mp.start();
 
@@ -192,7 +187,7 @@ public class AddControl extends Activity {
      * @param control
      * @return
      */
-    private Control postToAPI(Control control) {
+    private void postToAPI(Control control) {
         try {
             JSONObject controlsJson = new JSONObject();
             JSONObject controlJson = new JSONObject();
@@ -206,16 +201,40 @@ public class AddControl extends Activity {
 
             controlsJson.put("control", controlJson);
             System.out.println(controlsJson.toString());
+            /**
+             *
+             */
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost("http://192.168.1.144:5000/api/controls");
+            StringEntity entity = new StringEntity(controlsJson.toString());
+            entity.setContentType("application/json;charset=UTF-8");
+            entity.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json;charset=UTF-8"));
+            //Setting the content type is very important
+//            entity.setContentEncoding(HTTP.UTF_8);
+//            entity.setContentType("application/json");
+            httpPost.setEntity(entity);
 
-            connection();
+            //Execute and get the response.
+            HttpResponse response = httpClient.execute(httpPost);
+            /**
+             *
+             */
 
-//            StringEntity se = new StringEntity(controlsJson.toString());
-            post.setEntity(new StringEntity(controlsJson.toString(), "UTF-8"));
-            post.setHeader("Accept", "application/json");
-            post.setHeader("Content-type", "application/json");
-//            System.out.println(post.getEntity().toString());
-
-            HttpResponse response = client.execute(post);
+//            HttpPost post = new HttpPost("http://192.168.1.144:5000/api/controls/");
+//            post.setHeader("Accept", "application/json");
+//            post.setHeader("Content-type", "application/json");
+////            StringEntity se = new StringEntity(controlsJson.toString());
+//            StringEntity entity = new StringEntity(controlsJson.toString(), HTTP.UTF_8);
+//            entity.setContentType("application/json");
+//
+//            post.setEntity(entity);
+//            post.addHeader("Accept", "application/json");
+//            post.addHeader("Content-type", "application/json");
+////            System.out.println(post.getEntity().toString());
+//
+//            HttpClient client = new DefaultHttpClient();
+//            HttpResponse response = client.execute(post);
+//            Log.d("HTTP", "HTTP: OK");
 
 //            String responseText = EntityUtils.toString(response.getEntity());
 //            JSONObject json = new JSONObject(responseText);
@@ -226,7 +245,7 @@ public class AddControl extends Activity {
 
 //        control.setId(45);
 
-        return control;
+//        return control;
     }
 
     private void showDate() {
